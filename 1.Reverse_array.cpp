9a85e653553450b868cfdcfa8c65be791a99a62e
Ritik Gupta <gritik95@gmail.com>
2020-12-24 12:15:55 +0530

    add individual question links

--- 1.Reverse_array.cpp
+++ 1.Reverse_array.cpp
@@ -1,3 +1,4 @@
+// https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
 #include<iostream>
 using namespace std;
 
--- 2.Min_max.cpp
+++ 2.Min_max.cpp
@@ -1,7 +1,4 @@
-/*
-    Write a C function to return minimum and maximum in an array. 
-    Your program should make the minimum number of comparisons. 
-*/
+// https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/
 
 #include<iostream>
 using namespace std;
